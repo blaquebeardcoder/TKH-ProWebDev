8c4a5cf3b4034fbe47987d723712d2ae62f4d417
TKH Student 1 <tkh@TKHs-Air-2.home>
2020-11-01 18:00:44 -0500

    First codealong

---
+++ Week_6/Project - toDoList/readme.md
@@ -0,0 +1,14 @@
+**To-Do List Project Overview**
+
+As a part of our learning and comprehension of Javascript objects, we were tasked with building a small to-do list web application using OOP.  The criteria is as follows;
+
+
+1. Must make use of _Object Oriented Programming_ in the information used on the flashcards. 
+
+2. CSS must demonstrate a _minimal level of competency_, it cannot be left as black and white. 
+
+3. Plus - while the added functionality of the user typing into the flashcards the information is nice but not a must. 
+Javascript constructor functions must be utilized in this project even if it’s minimal.
+
+4. No Jquery allowed. 
+ 
---
+++ Week_6/Project - toDoList/todo.js
@@ -0,0 +1,91 @@
+const container = document.querySelector('.container');
+let inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
+
+if(window.localStorage.getItem("todos") == undefined){
+     let todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+let todosEX = window.localStorage.getItem("todos");
+let todos = JSON.parse(todosEX);
+
+
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	let input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('itemInput');
+
+    	let edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = "EDIT";
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	let remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "REMOVE";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
+
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
+
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
+}
+
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.this == 13){
+		check();
+	}
+})
+
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
+}
+
+
+for (let v = 0 ; v < todos.length ; v++){
+    new item(todos[v]);
+}
+
+
+new item("Learn Vanilla JS");
+new item("Practice Udemy tutorials");
+
+//Code is modified from codealong with YouTuber "TrueCoder"
